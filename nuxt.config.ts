2969a1173faac3180bf09066cc69fdc2581e9b04
Simone Romano <me@simoneromano.com>
2023-05-03 11:40:59 +0300

    Add external javascript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      script: [
+        {
+          hid: "my_external_javascript",
+          src: "my_external_javascript.js",
+        },
+      ],
+    },
+  },
+});
---
+++ public/my_external_javascript.js
@@ -0,0 +1,1 @@
+alert("External javascript loaded.");
